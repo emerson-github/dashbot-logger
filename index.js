f921d5191cdcb94b793f009e260b82b7cea2fcc8
shotamakino <shota@dashbot.io>
2019-02-12 10:51:34 -0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,26 @@
+const { isLambda } = require('./util');
+
+function CloudWatchLogger(options) {
+  if (!isLambda) {
+    throw new Error('Log option only supports AWS Lambda functions.')
+  }
+
+  this._loggerStream = new CloudWatchStreamLogger(options)
+
+  this.log = (value) => {
+    var that = this;
+
+    if (_.isObject(value)) {
+      value = JSON.stringify(value)
+    } else {
+      value = value.toString()
+    }
+
+    that._loggerStream.write({
+      timestamp: new Date().getTime(),
+      message: value
+    })
+  }
+}
+
+module.exports = CloudWatchLogger
